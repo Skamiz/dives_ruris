8562371bb388ca7b18539fa6f5b20612fedcead7
Skamiz <48406239+Skamiz@users.noreply.github.com>
2024-03-18 15:02:51 +0100

    update xjoined for better wall connectivity

--- mods/xjoined/api.lua
+++ mods/xjoined/api.lua
@@ -20,6 +20,14 @@ local wall_prototype = {
 	}
 }
 
+local nodebox_wall_tall = {
+	type = "connected",
+	fixed = {-4/16, -8/16, -4/16, 4/16, 8/16, 4/16},
+	connect_front = {-3/16, -8/16, -8/16, 3/16, 8/16, -4/16},
+	connect_left = {-8/16, -8/16, -3/16, -4/16, 8/16, 3/16},
+	connect_back = {-3/16, -8/16, 4/16, 3/16, 8/16, 8/16},
+	connect_right = {4/16, -8/16, -3/16, 8/16, 8/16, 3/16},
+}
 local nodebox_wall_nopost = {
 	type = "connected",
 	fixed = {-3/16, -8/16, -3/16, 3/16, 6/16, 3/16},
@@ -28,11 +36,25 @@ local nodebox_wall_nopost = {
 	connect_back = {-3/16, -8/16, 3/16, 3/16, 6/16, 8/16},
 	connect_right = {3/16, -8/16, -3/16, 8/16, 6/16, 3/16},
 }
+local nodebox_wall_tall_nopost = {
+	type = "connected",
+	fixed = {-3/16, -8/16, -3/16, 3/16, 8/16, 3/16},
+	connect_front = {-3/16, -8/16, -8/16, 3/16, 8/16, -3/16},
+	connect_left = {-8/16, -8/16, -3/16, -3/16, 8/16, 3/16},
+	connect_back = {-3/16, -8/16, 3/16, 3/16, 8/16, 8/16},
+	connect_right = {3/16, -8/16, -3/16, 8/16, 8/16, 3/16},
+}
+
+local function is_walkable(pos)
+	local node = minetest.get_node(pos)
+	return minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].walkable
+end
 
 function xjoined.register_wall(name, def)
+	-- wall with post
 	for k, v in pairs(wall_prototype) do
 		if def[k] == nil then
-			-- if type(v) == "table" then v = teble.copy(v)
+			-- if type(v) == "table" then v = table.copy(v)
 			def[k] = v
 		end
 	end
@@ -45,11 +67,16 @@ function xjoined.register_wall(name, def)
 			minetest.node_punch(pos, node, puncher, pointed_thing)
 		end
 	end
+	def._on_update = function(pos)
+		local u_pos = vector.new(pos.x, pos.y + 1, pos.z)
+		if is_walkable(u_pos) then
+			minetest.set_node(pos, {name = name .. "_tall"})
+		end
+	end
 
+	-- wall without post
 	local def_np = table.copy(def)
-
-	minetest.register_node(name, def)
-
+	def_np.description = def.description .. " (no post)"
 	def_np.drop = name
 	def_np.groups.not_in_creative_inventory = 1
 	def_np.node_box = nodebox_wall_nopost
@@ -60,7 +87,53 @@ function xjoined.register_wall(name, def)
 			minetest.node_punch(pos, node, puncher, pointed_thing)
 		end
 	end
+	def_np._on_update = function(pos)
+		local u_pos = vector.new(pos.x, pos.y + 1, pos.z)
+		if is_walkable(u_pos) then
+			minetest.set_node(pos, {name = name .. "_tall_nopost"})
+		end
+	end
+
+	-- tall wall with post
+	local def_t = table.copy(def_np)
+	def_t.description = def.description .. " (tall)"
+	def_t.node_box = nodebox_wall_tall
+	def_t.on_punch = function(pos, node, puncher, pointed_thing)
+		if puncher:get_wielded_item():get_name() == name then
+			minetest.set_node(pos, {name = name .. "_tall_nopost"})
+		else
+			minetest.node_punch(pos, node, puncher, pointed_thing)
+		end
+	end
+	def_t._on_update = function(pos)
+		local u_pos = vector.new(pos.x, pos.y + 1, pos.z)
+		if not is_walkable(u_pos) then
+			minetest.set_node(pos, {name = name})
+		end
+	end
+
+	-- tall wall without post
+	local def_t_np = table.copy(def_t)
+	def_t_np.description = def.description .. " (tall, no post)"
+	def_t_np.node_box = nodebox_wall_tall_nopost
+	def_t_np.on_punch = function(pos, node, puncher, pointed_thing)
+		if puncher:get_wielded_item():get_name() == name then
+			minetest.set_node(pos, {name = name .. "_tall"})
+		else
+			minetest.node_punch(pos, node, puncher, pointed_thing)
+		end
+	end
+	def_t_np._on_update = function(pos)
+		local u_pos = vector.new(pos.x, pos.y + 1, pos.z)
+		if not is_walkable(u_pos) then
+			minetest.set_node(pos, {name = name .. "_nopost"})
+		end
+	end
+
+	minetest.register_node(name, def)
 	minetest.register_node(name .. "_nopost", def_np)
+	minetest.register_node(name .. "_tall", def_t)
+	minetest.register_node(name .. "_tall_nopost", def_t_np)
 end
 
 function xjoined.register_wall_recipe(output, input)
